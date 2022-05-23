1f92a19f76b93f95dbb0b03e3b8213f9d0bea658
Eason Chen <arx827@gmail.com>
2022-05-24 02:40:44 +0800

    [feature] 『』

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,133 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+const items = [
+  {
+    label: "File",
+    icon: "pi pi-fw pi-file",
+    items: [
+      {
+        label: "New",
+        icon: "pi pi-fw pi-plus",
+        items: [
+          {
+            label: "Bookmark",
+            icon: "pi pi-fw pi-bookmark",
+          },
+          {
+            label: "Video",
+            icon: "pi pi-fw pi-video",
+          },
+        ],
+      },
+      {
+        label: "Delete",
+        icon: "pi pi-fw pi-trash",
+      },
+      {
+        separator: true,
+      },
+      {
+        label: "Export",
+        icon: "pi pi-fw pi-external-link",
+      },
+    ],
+  },
+  {
+    label: "Edit",
+    icon: "pi pi-fw pi-pencil",
+    items: [
+      {
+        label: "Left",
+        icon: "pi pi-fw pi-align-left",
+      },
+      {
+        label: "Right",
+        icon: "pi pi-fw pi-align-right",
+      },
+      {
+        label: "Center",
+        icon: "pi pi-fw pi-align-center",
+      },
+      {
+        label: "Justify",
+        icon: "pi pi-fw pi-align-justify",
+      },
+    ],
+  },
+  {
+    label: "Users",
+    icon: "pi pi-fw pi-user",
+    items: [
+      {
+        label: "New",
+        icon: "pi pi-fw pi-user-plus",
+      },
+      {
+        label: "Delete",
+        icon: "pi pi-fw pi-user-minus",
+      },
+      {
+        label: "Search",
+        icon: "pi pi-fw pi-users",
+        items: [
+          {
+            label: "Filter",
+            icon: "pi pi-fw pi-filter",
+            items: [
+              {
+                label: "Print",
+                icon: "pi pi-fw pi-print",
+              },
+            ],
+          },
+          {
+            icon: "pi pi-fw pi-bars",
+            label: "List",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Events",
+    icon: "pi pi-fw pi-calendar",
+    items: [
+      {
+        label: "Edit",
+        icon: "pi pi-fw pi-pencil",
+        items: [
+          {
+            label: "Save",
+            icon: "pi pi-fw pi-calendar-plus",
+          },
+          {
+            label: "Delete",
+            icon: "pi pi-fw pi-calendar-minus",
+          },
+        ],
+      },
+      {
+        label: "Archieve",
+        icon: "pi pi-fw pi-calendar-times",
+        items: [
+          {
+            label: "Remove",
+            icon: "pi pi-fw pi-calendar-minus",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Quit",
+    icon: "pi pi-fw pi-power-off",
+  },
+];
 </script>
 
 <template>
+  <!-- <Menubar :model="items" /> -->
   <header>
     <img
       alt="Vue logo"
@@ -27,7 +151,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 </template>
 
 <style>
-@import "@/assets/base.css";
+/* @import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
@@ -121,5 +245,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
+import "bootstrap";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+import Menubar from "primevue/menubar";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(PrimeVue);
 app.use(router);
+
+app.component(Menubar.name, Menubar);
 
 app.mount("#app");
